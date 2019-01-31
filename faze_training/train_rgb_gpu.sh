24611f1ecc6c6f1a36667267154cf4085de7acfb
Abdullahaml1 <alex039u2@login01.local>
2019-02-01 00:24:00 +0200

    Adding Faze training script.

--- faze_training/train_rgb_gpu.sh
+++ faze_training/train_rgb_gpu.sh
@@ -13,7 +13,7 @@ source ~/data/anaconda3/bin/activate
 export LD_PRELOAD="/home/alex039u2/data/anaconda3/glibc-2.14/lib/libc.so.6"
 module load CUDA/8.0.61
 
-cd ~/data/tsn_paper/server_scripts/faze_tsn_pytorch
+cd ~/data/tsn_paper/server_scripts/real-time-action-recognition/
 
 echo "Start Training ..................................."
 
