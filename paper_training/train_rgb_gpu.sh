41a2c61ae0f91dee5ad76ca4cd9aefbb0e1774ac
Abdullahaml1 <alex039u2@login01.local>
2019-02-04 22:55:23 +0200

    Updating batch size from 256 to 128.

--- paper_training/train_rgb_gpu.sh
+++ paper_training/train_rgb_gpu.sh
@@ -20,7 +20,7 @@ echo "Start Training ..................................."
 python3 main.py ucf101 RGB /home/alex039u2/data/tsn_paper/datasets/rgb_train_FileList1.txt /home/alex039u2/data/tsn_paper/datasets/rgb_test_FileList1.txt \
    --arch resnet18 --num_segments 3 \
    --gd 20 --lr 0.001 --lr_steps 30 60 --epochs 60 \
-   -b 256 -j 2 --dropout 0.8 \
+   -b 128 -j 2 --dropout 0.8 \
    --gpus 0 1 --snapshot_pref /home/alex039u2/data/tsn_paper/server_scripts/paper_training/ucf101_RGB_resnet18_
 
 
