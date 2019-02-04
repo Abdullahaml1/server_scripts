52c45f6b07e68375b36d2c00ad3ca67449bcf2d1
Abdullahaml1 <alex039u2@login01.local>
2019-02-04 21:46:45 +0200

    Updating model from BNincption to resnet18.

--- paper_training/train_rgb_gpu.sh
+++ paper_training/train_rgb_gpu.sh
@@ -18,7 +18,7 @@ cd ~/data/tsn_paper/server_scripts/faze_tsn_pytorch
 echo "Start Training ..................................."
 
 python3 main.py ucf101 RGB /home/alex039u2/data/tsn_paper/datasets/rgb_train_FileList1.txt /home/alex039u2/data/tsn_paper/datasets/rgb_test_FileList1.txt \
-   --arch BNInception --num_segments 3 \
+   --arch resnet18 --num_segments 3 \
    --gd 20 --lr 0.001 --lr_steps 30 60 --epochs 60 \
    -b 256 -j 2 --dropout 0.8 \
    --gpus 0 1 --snapshot_pref /home/alex039u2/data/tsn_paper/server_scripts/paper_training/ucf101_RGB_resnet18_
