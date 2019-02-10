35b35606bc639f59fc9926309bb6a2e2dae0f177
Abdullahaml1 <alex039u2@login01.local>
2019-02-10 23:54:59 +0200

    training script for first rgb_diff.

--- faze_training/RGB_diff/train_rgb_gpu.sh
+++ faze_training/RGB_diff/train_rgb_gpu.sh
@@ -17,11 +17,11 @@ cd ~/data/tsn_paper/server_scripts/real-time-action-recognition/
 
 echo "Start Training ..................................."
 
-python3 main.py ucf101 RGB /home/alex039u2/data/tsn_paper/datasets/rgb_train_FileList1.txt /home/alex039u2/data/tsn_paper/datasets/rgb_test_FileList1.txt \
-   --arch resnet18 --num_segments 3 \
-   --gd 20 --lr 0.001 --lr_steps 30 60 --epochs 60 \
-   -b 256 -j 2 --dropout 0.8 \
-   --gpus 0 1 --snapshot_pref /home/alex039u2/data/tsn_paper/server_scripts/faze_training/ucf101_resnet18_256_
+python3 main.py ucf101 RGBDiff /home/alex039u2/data/tsn_paper/datasets/rgb_train_FileList1.txt /home/alex039u2/data/tsn_paper/datasets/rgb_test_FileList1.txt \
+   --arch  BNInception --num_segments 7 \
+   --gd 40 --lr 0.001 --lr_steps 80 160 --epochs 180 \
+   -b 128 -j 2 --dropout 0.8 \
+   --gpus 0 1 --snapshot_pref /home/alex039u2/data/tsn_paper/server_scripts/faze_training/RGB_diff/ucf101_diff_bninception_128_
 
 
 
