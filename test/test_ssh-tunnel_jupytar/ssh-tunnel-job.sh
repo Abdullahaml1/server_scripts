c859bd760122900f975992ad3bf33fb4038338f8
Abdullahaml1 <alex039u2@login01.local>
2019-02-21 18:44:20 +0200

    first try.

--- test/test_ssh-tunnel_jupytar/ssh-tunnel-job.sh
+++ test/test_ssh-tunnel_jupytar/ssh-tunnel-job.sh
@@ -2,17 +2,23 @@
 
 
 
-#SBATCH --job-name=tes_gpus_number
+#SBATCH --job-name=test-ssh-tunnel
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:2
 #SBATCH --nodes=2
 #SBATCH --ntasks=2
-#SBATCH --time=00:15:00
+#SBATCH --time=2:00:00
 
 source ~/data/anaconda3/bin/activate
 export LD_PRELOAD="/home/alex039u2/data/anaconda3/glibc-2.14/lib/libc.so.6"
 module load CUDA/8.0.61
 
+hostname -I
+netstat -antu
 
-cd ~/data/tsn_paper/server_scripts/test/gpus/
-python3 test_gpus.py 
+ssh -R localhost:6666:localhost:6666 head2 -N
+
+cd ~/data/tsn_paper/server_scripts/streaming_codes
+
+python3 server.py
+
