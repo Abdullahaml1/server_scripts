d649083801dc9b42146b037f42cdec0108bf8d82
Abdullahaml1 <alex039u2@login01.local>
2019-03-12 16:56:53 -0400

    Training took 2-01:20:15 day, Testing took 05:40:59 with  80.20%.

---
+++ faze_training/RGB_diff/results/180-epoch-7-seg/slurm-114/train_rgb_diff_gpu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+
+#SBATCH --job-name=train_FAZE_RGB_diff_BNinception_UCF101
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --time=180:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=12
+
+source ~/data/anaconda3/bin/activate
+module load CUDA
+
+cd ~/data/tsn_paper/server_scripts/real-time-action-recognition/
+
+
+echo "Start Training ..................................."
+
+python3 -u  main.py ucf101 RGBDiff ~/data/tsn_paper/datasets/rgb_train_FileList1.txt ~/data/tsn_paper/datasets/rgb_test_FileList1.txt \
+   --arch  BNInception --num_segments 7 \
+   --gd 40 --lr 0.001 --lr_steps 80 160 --epochs 180 \
+   -b 64 -j 12 --dropout 0.8 \
+   --gpus 0 1 \
+   --snapshot_pref ~/data/tsn_paper/server_scripts/faze_training/RGB_diff/UCF101-7seg
+
+
+
+echo "End of Training ................................."
+
