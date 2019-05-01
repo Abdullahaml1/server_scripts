b07d8a91b9af0d02940de62bd4d90cf066eced34
PlebChan <47537257+PlebChan@users.noreply.github.com>
2019-05-01 22:12:45 +0200

    removing key parser (after implementing paramiko)

--- Testing_all_system/run_code_slurm.sh
+++ Testing_all_system/run_code_slurm.sh
@@ -20,6 +20,6 @@ echo "Starting System  ..................................."
 
 python3 -u polymerization.py ucf101 \
 ~/data/tsn_paper/server_scripts/weights/RGB/kinetics/Kinetics_BNInception__rgb_model_best.pth.tar ~/data/tsn_paper/server_scripts/weights/RGB_diff/180epoch-3seg-128batch-kinetics_flow/UCF101-3seg_rgbdiff_kinetics_flow_model_best.pth.tar \
---arch BNInception --classInd_file UCF_lists/classInd.txt -j 2 --gpus 0 1 --test_segments 3 --psi 0 --key C:/Users/user/AppData/Roaming/SPB_16.6/.ssh/id_rsa_aml
+--arch BNInception --classInd_file UCF_lists/classInd.txt -j 2 --gpus 0 1 --test_segments 3 --psi 0 --h login01 --test
 
 
