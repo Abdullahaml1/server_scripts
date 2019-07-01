3be3f1b11432f3a7a5355eabf6cf333689951e6b
Abdullahaml1 <alex039u2@login01.c2.hpc.bibalex.org>
2019-07-01 11:59:19 -0400

    replacing check point with best model.

--- faze_training/RGB_diff/test_rgb_diff_gpu.sh
+++ faze_training/RGB_diff/test_rgb_diff_gpu.sh
@@ -17,7 +17,7 @@ cd ~/data/tsn_paper/real-time-action-recognition/
 
 echo "Start Testing ..................................."
 
-python3 -u  test_models.py ucf101 RGBDiff ~/data/tsn_paper/datasets/rgb_test_FileList1.txt ~/data/tsn_paper/server_scripts/faze_training/RGB_diff/results/180-epoch-3-seg/_rgbdiff_checkpoint.pth.tar \
+python3 -u  test_models.py ucf101 RGBDiff ~/data/tsn_paper/datasets/rgb_test_FileList1.txt ~/data/tsn_paper/server_scripts/faze_training/RGB_diff/results/180-epoch-3-seg/_rgbdiff_model_best.pth.tar \
    --arch  BNInception \
    --classInd_file UCF_lists/classInd.txt \
    --save_scores ~/data/tsn_paper/server_scripts/faze_training/RGB_diff/scores_RGBdiff_BNinception \
